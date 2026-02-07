f9ebc4bfd5b17c3e81dca2d3462d0495b7d59eb2
otavs <daviotaviano1@gmail.com>
2026-02-07 17:50:48 -0300

    feat: add env vars & dist

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import 'dotenv/config'
+
+import { router } from './routes/index.js';
 
 const server = express();
 
-server.get('/', (req, res) => {
-  return res.send('Ola');
-});
 
+server.use(express.json());
+
+
+server.use(router);
 
 export {server};
