6dbe1716200764bfb9b07052d6c3d951e8833077
otavs <daviotaviano1@gmail.com>
2026-02-08 18:02:34 -0300

    feat: validated with yup

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,13 +1,32 @@
 import type { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 export interface Icitie{
   name: string;
 }
 
+const bodyValidation: yup.ObjectSchema<Icitie> = yup.object().shape({
+  name: yup.string().required().min(3)
+})
+
 export const create = async(req: Request<{}, {}, Icitie>, res: Response) => {
+  let validatedData: Icitie | undefined = undefined;
 
-  const data = req.body;
-  console.log(data);
+  try {
+    validatedData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
 
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: yupError.message
+      }
+    })
+    
+  }
+
+  console.log(validatedData)
   return res.send('Create')
 }
+
