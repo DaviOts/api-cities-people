ba1c7e58a97a1183333709013bebd34f24676e5f
otavs <daviotaviano1@gmail.com>
2026-02-17 15:33:58 -0300

    feat: add njk support

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,15 +1,30 @@
-import express from 'express';
-import 'dotenv/config'
+import express from "express";
+import "dotenv/config";
+import nunjucks from "nunjucks";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import './shared/services/translationsYup.js'
-import { router } from './routes/index.js';
+import "./shared/services/translationsYup.js";
+import { router } from "./routes/index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const rootDir = path.resolve(__dirname, "..", "..");
 
 const server = express();
 
 
-server.use(express.json());
+nunjucks.configure(path.resolve(rootDir, "views"), {
+  autoescape: true, 
+  express: server, 
+  watch: true, 
+});
+server.set("view engine", "njk");
 
+server.use(express.static(path.resolve(rootDir, "public")));
+
+server.use(express.json());
 
 server.use(router);
 
-export {server};
+export { server };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,12 +1,49 @@
 import { Router } from "express";
 
-import { citiesController, peopleController, usersController } from "../controllers/index.js";
+import {
+  citiesController,
+  peopleController,
+  usersController,
+} from "../controllers/index.js";
 import { ensureAuthenticated } from "../shared/middleware/index.js";
+import { Knex } from "../database/knex/index.js";
+import { ETableNames } from "../database/ETableNames.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  return res.send("Ola");
+
+
+router.get("/", async (req, res) => {
+  const [{ count: totalCities }] = (await Knex(ETableNames.CITIES).count<
+    [{ count: number }]
+  >("* as count")) as unknown as [{ count: number }];
+  const [{ count: totalPeople }] = (await Knex(ETableNames.PEOPLE).count<
+    [{ count: number }]
+  >("* as count")) as unknown as [{ count: number }];
+
+  return res.render("index", { totalCities, totalPeople });
+});
+
+
+router.get("/view/cities", async (req, res) => {
+  const cities = await Knex(ETableNames.CITIES).select("*");
+  const totalCities = cities.length;
+
+  return res.render("cities", { cities, totalCities });
+});
+
+
+router.get("/view/people", async (req, res) => {
+  const people = await Knex(ETableNames.PEOPLE)
+    .select(`${ETableNames.PEOPLE}.*`, `${ETableNames.CITIES}.name as cityName`)
+    .leftJoin(
+      ETableNames.CITIES,
+      `${ETableNames.PEOPLE}.cityId`,
+      `${ETableNames.CITIES}.id`,
+    );
+  const totalPeople = people.length;
+
+  return res.render("people", { people, totalPeople });
 });
 
 router.get(
@@ -39,7 +76,6 @@ router.delete(
   citiesController.deleteByIdValidation,
   citiesController.deleteById,
 );
-
 
 router.get(
   "/people",
