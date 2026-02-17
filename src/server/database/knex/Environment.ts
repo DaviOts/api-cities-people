1e69ab66d2456ce0c343b68548ec112444fa7092
otavs <daviotaviano1@gmail.com>
2026-02-17 15:29:05 -0300

    feat: setup Postgres environment

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -2,38 +2,36 @@ import { Knex } from "knex";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+dotenv.config({
+  path: path.resolve(__dirname, "..", "..", "..", "..", ".env"),
+});
+
 export const development: Knex.Config = {
-  client: "sqlite3",
-  useNullAsDefault: true,
-  connection: {
-    filename: path.resolve(
-      __dirname,
-      "..",
-      "..",
-      "..",
-      "..",
-      "database.sqlite",
-    ),
-  },
+  client: "pg",
   migrations: {
     directory: path.resolve(__dirname, "..", "migrations"),
   },
   seeds: {
     directory: path.resolve(__dirname, "..", "seeds"),
   },
-  pool: {
-    afterCreate: (connection: any, done: any) => {
-      connection.run("PRAGMA foreign_keys = ON", done);
-    },
+  connection: {
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME || "postgres",
+    port: Number(process.env.DB_PORT) || 5432,
   },
 };
 
 export const test: Knex.Config = {
   ...development,
+  client: "sqlite3",
+  useNullAsDefault: true,
   connection: ":memory:",
 };
 
